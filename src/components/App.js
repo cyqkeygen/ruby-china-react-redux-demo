96297a29f1985c9d023182ed26c4455b28c011c5
Yuquan Chen <cyqsdy@gmail.com>
2017-08-01 00:59:32 +0800

    seperate Home component to a single file

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
+import Home from './Home';
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom';
-
-class Home extends React.Component {
-  render(){
-    return (
-      <div>Home</div>
-    )
-  }
-}
 
 class Topics extends React.Component {
   render(){
