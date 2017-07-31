7f71ef78481b6edae368ba5074b7490fcccb10e3
Yuquan Chen <cyqsdy@gmail.com>
2017-08-01 01:13:33 +0800

    Add Header component and remove the relative code from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,72 +1,17 @@
 import React from 'react';
-import Home from './Home';
+import Header from './Header';
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom';
 
-class Topics extends React.Component {
-  render(){
-    return (
-      <div>Topics</div>
-    )
-  }
-}
-
-class Tops extends React.Component {
-  render(){
-    return (
-      <div>Tops</div>
-    )
-  }
-}
-
-class Jobs extends React.Component {
-  render(){
-    return (
-      <div>Jobs</div>
-    )
-  }
-}
-
-class Wiki extends React.Component {
-  render(){
-    return (
-      <div>Wiki</div>
-    )
-  }
-}
-
-class Sites extends React.Component {
-  render(){
-    return (
-      <div>Sites</div>
-    )
-  }
-}
-
 class App extends React.Component {
   render(){
     return (
-      <Router>
-        <div>
-          <ul>
-            <li><Link to='/'>首页</Link></li>
-            <li><Link to='/topics'>社区</Link></li>
-            <li><Link to='/tops'>头条</Link></li>
-            <li><Link to='/jobs'>招聘</Link></li>
-            <li><Link to='/wiki'>Wiki</Link></li>
-            <li><Link to='/sites'>酷站</Link></li>
-          </ul>
-          <Route exact path='/' component={Home}/>
-          <Route path='/topics' component={Topics}/>
-          <Route path='/tops' component={Tops}/>
-          <Route path='/jobs' component={Jobs}/>
-          <Route path='/wiki' component={Wiki}/>
-          <Route path='/sites' component={Sites}/>
-        </div>
-      </Router>
+      <div>
+        <Header />
+      </div>
     )
   }
 }
