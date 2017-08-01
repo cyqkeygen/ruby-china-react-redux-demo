b1d377e10c690685e63f4a1524978ea5f3c95324
Yuquan Chen <cyqsdy@gmail.com>
2017-08-01 16:46:41 +0800

    add content to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
-  render(){
+
+  constructor() {
+    super();
+    this.state = {
+      popularTopics: []
+    }
+  }
+  componentWillMount() {
+    this.loadPopularTopics();
+  }
+
+  loadPopularTopics() {
+  }
+
+  renderLinks() {
     return (
-      <div>Home</div>
+      <div>
+        <Link to='/topics'>Ruby 社区</Link>
+        <Link to='/wiki'>技术文档</Link>
+        <Link to='/jobs'>招聘与求职</Link>
+        <Link to='/topics/popular'>精华文章</Link>
+      </div>
+    )
+  }
+
+  renderPopularTopics() {
+    const { popularTopics } = this.state;
+    return (
+      <div>
+        <p>社区精华贴</p>
+        {popularTopics.map( topic => <li key={topic.id}>topic.content</li>)}
+      </div>
+    );
+  }
+
+  render(){
+    const links = this.renderLinks();
+    const popularTopics = this.renderPopularTopics();
+    return (
+      <div>
+        <div>
+          <p>Ruby China 官方 RubyGems 镜像、Ruby 镜像 正式上线！</p>
+          <p>gem source -a https://gems.ruby-china.org</p>
+        </div>
+        {links}
+        {popularTopics}
+      </div>
     )
   }
 }
