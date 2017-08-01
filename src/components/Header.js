420468ed7c53c84b9be77144f05cf70b3c9a4659
Yuquan Chen <cyqsdy@gmail.com>
2017-08-01 14:58:46 +0800

    rewrite routes and links

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,31 +5,33 @@ import {
   Link
 } from 'react-router-dom';
 import Home from './Home';
+import Topics from './Topics';
+import Headlines from './Headlines';
 import Jobs from './Jobs';
 import Sites from './Sites';
-import Topics from './Topics';
-import Tops from './Tops';
 import Wiki from './Wiki';
+
+const routes = [
+  { path: '/', name: '首页', component: Home},
+  { path: '/topics', name: '社区', component: Topics},
+  { path: '/headlines', name: '头条', component: Headlines},
+  { path: '/jobs', name: '招聘', component: Jobs},
+  { path: '/wiki', name: 'Wiki', component: Wiki},
+  { path: '/sites', name: '酷站', component: Sites},
+];
 
 class Header extends React.Component {
   render(){
+    const links = routes.map(({path, name}, key) => <li key={key}><Link to={path}>{name}</Link></li>);
+    const routeComponents = routes.map(({path, component}, key) => path === '/'
+        ? <Route exact path={path} component={component} key={key} />
+        : <Route path={path} component={component} key={key} />
+    );
     return (
       <Router>
         <div>
-          <ul>
-            <li><Link to='/'>首页</Link></li>
-            <li><Link to='/topics'>社区</Link></li>
-            <li><Link to='/tops'>头条</Link></li>
-            <li><Link to='/jobs'>招聘</Link></li>
-            <li><Link to='/wiki'>Wiki</Link></li>
-            <li><Link to='/sites'>酷站</Link></li>
-          </ul>
-          <Route exact path='/' component={Home}/>
-          <Route path='/topics' component={Topics}/>
-          <Route path='/tops' component={Tops}/>
-          <Route path='/jobs' component={Jobs}/>
-          <Route path='/wiki' component={Wiki}/>
-          <Route path='/sites' component={Sites}/>
+          <ul>{links}</ul>
+          {routeComponents}
         </div>
       </Router>
     )
