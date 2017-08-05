e7e0001fae91def13c17a8f4adf47289c9173c0b
Yuquan Chen <cyqsdy@gmail.com>
2017-08-05 11:08:24 +0800

    initialize Topics component layout sytles and and image resources

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import styles from '../styles/modules/Topics.scss';
 
 class Topics extends React.Component {
-  render(){
+
+  renderTopics() {
     return (
-      <div>Topics</div>
+      <div className={styles['left-col']}>
+        col-left
+      </div>
+    )
+  }
+
+  renderRightColumn() {
+    return (
+      <div className={styles['right-col']}>
+        <div>
+          <button>
+            发布新话题
+          </button>
+        </div>
+        <div>
+          <div>小贴士</div>
+          <div>本站是开源的哦，访问 http://gethomeland.com 获得更多信息</div>
+        </div>
+        <div>
+          <div>推荐Ruby镜像</div>
+          <div>Ruby 镜像</div>
+          <div>RubyGems 镜像</div>
+          <div>Rails 视频教程</div>
+          <div>Rails Guides 中文版</div>
+          <div>Ruby on Rails 教程</div>
+        </div>
+        <div>
+          友情社区
+        </div>
+        <div>
+          统计信息
+        </div>
+      </div>
+    )
+  }
+
+  render(){
+    const topics = this.renderTopics();
+    const rightColumn = this.renderRightColumn();
+    console.log(topics)
+    return (
+      <div>
+        {topics}
+        {rightColumn}
+      </div>
     )
   }
 }
