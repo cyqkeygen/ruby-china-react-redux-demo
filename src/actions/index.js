a7674f3d53fbf807ff52629669abb4a210cd76ed
Yuquan Chen <cyqsdy@gmail.com>
2017-08-08 05:59:48 +0800

    coding actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,53 +1,112 @@
 import fetch from 'isomorphic-fetch';
+import { createActions } from 'redux-actions';
 
-export const REQUEST_TOPICS = 'REQUEST_TOPICS';
-export const RECEIVE_TOPICS = 'RECEIVE_TOPICS';
+export const SELECT_PAGE = 'SELECT_PAGE';
+export const REQUEST_TOPICS_BY_TYPE = 'REQUEST_TOPICS_BY_TYPE';
+export const RECEIVE_TOPICS_BY_TYPE = 'RECEIVE_TOPICS_BY_TYPE';
+export const REQUEST_TOPICS_BY_NODE = 'REQUEST_TOPICS_BY_NODE';
+export const RECEIVE_TOPICS_BY_NODE = 'RECEIVE_TOPICS_BY_NODE';
 export const USER_LOGIN = 'USER_LOGIN';
 export const USER_LOGOUT = 'USER_LOGOUT';
+const requestTopicsUrl = `https://ruby-china.org/api/v3/topics`;
 
-const requestTopicUrl = `https://ruby-china.org/api/v3/topics`;
+/* export const {
+  selectPage,
+} = createActions({
+  SELECT_PAGE: page => ({ page }),
+}); */
 
-export function requestTopics(topicType) {
+/*  
+ *  page enum ['home', 'topics','jobs']
+ *  */
+export function selectPage(page) {
   return {
-    type: REQUEST_TOPICS,
-    topicType 
+    type: SELECT_PAGE,
+    page
   }
 }
 
-export function receiveTopics(topicType, topics) {
+function identifyPositive(nodeId) {
+  return typeof nodeId === 'number' && nodeId > 0;
+}
+
+export function requestTopics(options = {
+  type: 'defalut',
+  nodeId: -1
+}) {
+  const { type, nodeId } = options;
+  if (identifyPositive(nodeId)) {
+    return {
+      type: REQUEST_TOPICS_BY_NODE,
+      nodeId: nodeId
+    }
+  } else {
+    return {
+      type: REQUEST_TOPICS_BY_TYPE,
+      topicsType: type
+    }
+  }
+}
+
+export function receiveTopics(options = {
+  type: 'default',
+  nodeId: -1,
+  topics: []
+}) {
+  const { type, nodeId, topics } = options;
+  if (topics.length <= 0) return;
+  if (identifyPositive(nodeId)) {
+    return {
+      type: RECEIVE_TOPICS_BY_NODE,
+      nodeId,
+      topics,
+      receiveAt: Date.now()
+    }
+  } else {
+    return {
+      type: RECEIVE_TOPICS_BY_TYPE,
+      topicsType: type,
+      topics,
+      receivedAt: Date.now()
+    }
+  }
   return {
     type: RECEIVE_TOPICS,
-    topicType,
+    page,
     topics,
     receivedAt: Date.now()
   }
 }
 
-export function fetchTopics(topicType) {
-  return dispatch => {
-    dispatch(requestTopics(topicType));
-    fetch(requestTopicUrl + `?type=${topicType}`)
-      .then(response => {
-        console.log(response);
-        return response.json();
-      })
-      .then(json => {
-        dispatch(receiveTopics(topicType, json.topics))
-      });
+const urlPayload = (type, nodeId = -1, options = {
+  limit: 25,
+  offset: 0
+}) => {
+  const { limit, offset } = options;
+  const defaultPayload = `?limit=${limit}&offset=${offset}`;
+  if (identifyPositive(nodeId)) {
+    return defaultPayload + `&node_id=${nodeId}`;
+  } else {
+    return defaultPayload + `&type=${type}`;
   }
 }
 
-export function fetchTopicsAsync(topicType) {
-  return async function(dispatch) {
-    dispatch(requestTopics(topicType));
-    const response = await fetch(requestTopicUrl + `?type=${topicType}`);
-    console.log(response);
-    dispatch(receiveTopics(topicType, response.json.topics));
-  }
-}
-
-export function shouldFetchTopics(state, topicType){
-  if (!topics) {
-    return true
-  }
+export const fetchTopics = (page = 'home', topicsInfo, options = {
+  limit: 25,
+  offset: 0
+})  => (dispatch, getState) => {
+  const { type, nodeId } = topicsInfo;
+  const url = requestTopicsUrl + urlPayload(type, nodeId, options);
+  dispatch(selectPage(page));
+  dispatch(requestTopics({type, nodeId}));
+  fetch(url)
+    .then(res => res.json())
+    .then(json => {
+      dispatch(receiveTopics({
+        nodeId,
+        type,
+        topics: json.topics
+      }));
+    }
+    );
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,19 @@
 import { combineReducers } from 'redux';
-import { REQUEST_TOPICS, RECEIVE_TOPICS } from '../actions';
+import { 
+  SELECT_PAGE,
+  REQUEST_TOPICS_BY_TYPE,
+  RECEIVE_TOPICS_BY_TYPE,
+  REQUEST_TOPICS_BY_NODE,
+  RECEIVE_TOPICS_BY_NODE,
+} from '../actions';
 
-const topicTypeMap = {
-  excellent: 'excellentTopics',
+function selectPage(state = 'home', action) {
+  switch (action.type) {
+    case SELECT_PAGE:
+      return action.page;
+    default:
+      return state
+  }
 }
 
 function topics(state = {
@@ -13,12 +24,14 @@ function topics(state = {
   limit: 20
 }, action) {
   switch (action.type) {
-    case REQUEST_TOPICS:
+    case REQUEST_TOPICS_BY_TYPE:
+    case REQUEST_TOPICS_BY_NODE:
       return {
         ...state,
         isFetching: true
       };
-    case RECEIVE_TOPICS:
+    case RECEIVE_TOPICS_BY_TYPE:
+    case RECEIVE_TOPICS_BY_NODE:
       return {
         ...state,
         isFetching: false,
@@ -30,11 +43,17 @@ function topics(state = {
   }
 }
 
-// unused
+function createSubReducer(state, action) {
+}
+
+/*
+ * action = {type: REQUEST_TOPICS_BY_TYPE, topicType: 'excellent'}
+ * action = {type: RECEIVE_TOPICS_BY_TYPE, topicType: 'excellent', topics: []}
+ * */
 function topicsByType(state = {}, action) {
   switch (action.type) {
-    case REQUEST_TOPICS:
-    case RECEIVE_TOPICS:
+    case REQUEST_TOPICS_BY_TYPE:
+    case RECEIVE_TOPICS_BY_TYPE:
       return {
         ...state,
         [action.topicType]: topics(state[action.topicType], action)
@@ -44,8 +63,60 @@ function topicsByType(state = {}, action) {
   }
 }
 
+/* 
+ *  action = {type: REQUEST_TOPICS_BY_NODE, nodeId: 1}
+ *  action = {type: RECEIVE_TOPICS_BY_NODE, nodeId: 1, topics: []}
+ * */
+function topicsByNode(state = {}, action) {
+  switch (action.type) {
+    case REQUEST_TOPICS_BY_NODE:
+    case RECEIVE_TOPICS_BY_NODE:
+      return {
+        ...state,
+        [action.nodeId]: topics(state[action.nodeId], action)
+      }
+      break;
+    
+    default:
+      
+  } 
+}
+
+function createReducer(reducerFunction, reducerName) {
+  return (state, action) => {
+    
+  }
+}
+function types(state = {}, action) {
+  switch(action.type) {
+    case REQUEST_TOPICS_BY_TYPE:
+    case RECEIVE_TOPICS_BY_TYPE:
+      return {
+        ...state,
+        [action.topicsType]: topics(state[action.topicsType], action)
+      };
+    default:
+      return state;
+  }
+}
+
+function nodes(state = {}, action) {
+  switch (action.type) {
+    case REQUEST_TOPICS_BY_NODE:
+    case RECEIVE_TOPICS_BY_NODE:
+      return {
+        ...state,
+        [action.nodeId]: topics(state[action.nodeId], action)
+      };
+    default:
+      return state;
+  }
+}
+
 const rootReducer = combineReducers({
-  excellentTopics: topics
+  selectPage,
+  types,
+  nodes
 });
 
 export default rootReducer;
